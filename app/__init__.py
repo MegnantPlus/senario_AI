d13f61682863c85826727fc366020df9337ebbd5
Jerry <nubyeuem@gmail.com>
2026-01-18 00:06:09 +0700

    CORS FIx

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_pymongo import PyMongo
+from flask_cors import CORS
 from app.config import Config
 
 mongo = PyMongo()
@@ -7,6 +8,9 @@ mongo = PyMongo()
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+    
+    # Cho phép mọi domain truy cập (Bỏ qua lỗi CORS)
+    CORS(app)
     
     mongo.init_app(app)
     
